a63256a38ed6f94f950bfa8c5955d786059dafc6
Bob Lail <bob.lail@cph.org>
2015-08-16 20:49:17 -0500

    [improvement] Allowed specifying logeater's target with an environment variable, DATABASE_URL (20m)

--- lib/logeater.rb
+++ lib/logeater.rb
@@ -5,9 +5,24 @@ require "logeater/version"
 require "yaml"
 require "erb"
 
-config_file = File.expand_path("../../db/config.yml", __FILE__)
-config = YAML.load(ERB.new(File.read(config_file)).result).with_indifferent_access
-ActiveRecord::Base.establish_connection config[ENV["RAILS_ENV"] || "development"]
+database_url = ENV["DATABASE_URL"] unless ENV["RAILS_ENV"] == "test"
+if database_url
+  uri = Addressable::URI.parse(database_url)
+  config = {
+    adapter: "postgresql",
+    encoding: "utf8",
+    min_messages: "WARNING",
+    database: uri.path[1..-1],
+    host: uri.host,
+    username: uri.user,
+    password: uri.password,
+    port: uri.port
+  }
+else
+  config_file = File.expand_path("../../db/config.yml", __FILE__)
+  config = YAML.load(ERB.new(File.read(config_file)).result).with_indifferent_access[ENV["RAILS_ENV"] || "development"]
+end
+ActiveRecord::Base.establish_connection config
 
 module Logeater
 end
