0cc6e206ac490bcf7fb9e4ca729ced05adfd3580
Bob Lail <bob.lail@cph.org>
2015-10-03 15:07:17 -0500

    [fix] Recovered when importing duplicate values (30m)

--- lib/logeater/request.rb
+++ lib/logeater/request.rb
@@ -5,5 +5,18 @@ module Logeater
   class Request < ActiveRecord::Base
     self.table_name = "requests"
 
+    def self.import(values)
+      # values have to be unique by uuid
+      values = values.uniq { |request| request.uuid }
+
+      super values
+    rescue PG::UniqueViolation, ActiveRecord::RecordNotUnique
+      # try again after skipping requests with existing uuids
+      existing_uuids = where(uuid: values.map(&:uuid)).pluck(:uuid)
+      values = values.reject { |request| existing_uuids.member?(request.uuid) }
+
+      super values
+    end
+
   end
 end
