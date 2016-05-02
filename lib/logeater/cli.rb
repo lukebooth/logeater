a4aed4f702d82ac0d5780ce4930e4e4f63996893
Bob Lail <bob.lailfamily@gmail.com>
2016-05-02 13:13:33 -0500

    [skip] Improved verbosity of output (10m)

--- lib/logeater/cli.rb
+++ lib/logeater/cli.rb
@@ -39,8 +39,17 @@ module Logeater
     desc "import APP FILES", "imports files"
     def import(app, *files)
       unless options[:force]
+        started_at = Time.now
         imported_files = Logeater::Request.where(app: app).pluck("DISTINCT logfile")
+        finished_at = Time.now
+
+        $stderr.puts " > \e[34mListed imported logfiles for \e[1m#{app}\e[0;34m in \e[1m%.2f\e[0;34m seconds\e[0m\n" % [
+          finished_at - started_at ]
+
+        files_count = files.count
         files.reject! { |file| imported_files.member? File.basename(files[0]) }
+
+        $stderr.puts "   \e[34;1m#{files_count}\e[0;34m files passed, \e[1m#{files_count - files.length}\e[0;34m removed as already imported.\e[0m\n\n"
       end
 
       if files.empty?
@@ -50,22 +59,27 @@ module Logeater
 
       started_all = Time.now
       files.each_with_index do |file, i|
-        $stderr.puts " > \e[34mImporting \e[1m#{File.basename(file)}\e[0;34m (%d of %d)\e[0m\n\n" % [
+        $stderr.puts " > \e[34mImporting \e[1m#{File.basename(file)}\e[0;34m (%d of %d)\e[0m\n" % [
           i + 1,
           files.length ]
 
         reader = Logeater::Reader.new(app, file, options.slice(:progress, :verbose))
-        reader.remove_existing_entries!
+
+        started_at = Time.now
+        count = reader.remove_existing_entries!
+        finished_at = Time.now
+
+        $stderr.puts "   \e[34mDeleted \e[1m%d\e[0;34m requests for #{file} in \e[1m%.2f\e[0;34m seconds\e[0m\n" % [
+          count,
+          finished_at - started_at ]
 
         started_at = Time.now
         count = reader.import
         finished_at = Time.now
 
-        $stderr.puts " > \e[34mImported \e[1m%d\e[0;34m requests in \e[1m%.2f\e[0;34m seconds (%d of %d)\e[0m\n\n" % [
+        $stderr.puts "   \e[34mImported \e[1m%d\e[0;34m requests in \e[1m%.2f\e[0;34m seconds\e[0m\n\n" % [
           count,
-          finished_at - started_at,
-          i + 1,
-          files.length ]
+          finished_at - started_at ]
       end
 
       finished_all = Time.now
